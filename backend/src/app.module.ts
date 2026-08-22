0ced8517e5a40f3c7d35030ede2e92c6a7cc018c
0xGaziin <gaziin208@gmail.com>
2026-08-22 20:08:05 -0300

    feat(users): implement user registration route with bcrypt and prisma

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
