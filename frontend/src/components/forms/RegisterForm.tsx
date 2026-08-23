f363e43add2657ed07e6119a915e8051c2e18e26
0xGaziin <gaziin208@gmail.com>
2026-08-23 16:53:12 -0300

    feat(frontend): add loading state, error handling, and redirection to RegisterForm

--- frontend/src/components/forms/RegisterForm.tsx
+++ frontend/src/components/forms/RegisterForm.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import axios from "axios"
+import { useRouter } from "next/navigation";
 
 import InputField from "../ui/InputField"
 import ButtonSubmit from "../ui/ButtonSubmit"
+import { useState } from "react";
 
 export default function RegisterForm() {
+  const router = useRouter();
+  const [message, setMessage] = useState("");
+
   const handleRegister = async (e: any) => {
     e.preventDefault();
 
@@ -15,21 +20,26 @@ export default function RegisterForm() {
     const password = formData.get('password');
 
     try {
+      setMessage('Loading...');
       const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/users/register`, {
         name: name,
         email: email,
         password: password,
       });
+      setMessage('Finished! Redirecting...');
 
-      console.log(response.data)
+      console.log(response.data);
+      router.push('/login');
     } catch (error: any) {
-
+      console.error(error);
+      setMessage(error.response?.data?.message || 'Something went wrong.');
     }
   }
 
   return (
     <div>
       <h2 className="font-bold text-center text-2xl">Register</h2>
+      <p className={`font-bold ${message.includes('wrong') || message.includes('use') || message.includes('invalid') ? 'text-red-600' : 'text-green-600'}`}>{message}</p>
       <form className="flex flex-1 flex-col gap-2 items-center justify-center mt-2 mb-5" onSubmit={handleRegister}>
         <InputField 
           name="name"
